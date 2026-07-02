fba8afc452ff244f55189cd9df6be76c43c75c75
egpennington <egpennington@hotmail.com>
2026-07-02 10:34:12 -0800

    add header, footer, starter page and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-// React import isn't needed with the Vite/modern JSX transform
-// unless importing something like useState or useEffect.
-
 import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 import Splash from './pages/Splash'
 import Store from './pages/Store'
 import Admin from './pages/Admin'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Splash />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Layout>
   )
 }
 
---
+++ src/data/starterItem.js
@@ -0,0 +1,28 @@
+const sampleItems = [
+  {
+    id: 1,
+    title: 'Cordless Drill',
+    description: 'Good condition. Comes with battery and charger.',
+    price: 45,
+    image: '',
+    status: 'published',
+  },
+  {
+    id: 2,
+    title: 'Camping Chair',
+    description: 'Folding chair, lightly used.',
+    price: 15,
+    image: '',
+    status: 'published',
+  },
+  {
+    id: 3,
+    title: 'Shop Light',
+    description: 'Works great. Bright LED light.',
+    price: 25,
+    image: '',
+    status: 'sold',
+  },
+]
+
+export default sampleItems
